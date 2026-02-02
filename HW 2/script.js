a0174d12e522af39a060888c8e0fdf2c61a8ca64
LexiNunnally627 <agn2004@icloud.com>
2026-02-01 20:07:45 -0700

    HW 2

---
+++ HW 2/script.js
@@ -0,0 +1,7 @@
+console.log("My favorite websites:Youtube, Spotify, and Instagram");
+console.log("My favorite games: Sims 4, Minecraft, and Until Dawn");
+console.log("My favorite artists: Ariana Grande, I-DLE, and Twice-In my life music makes up a large part of the art I consume. All of these women have made great accomplishments in their craft. Ariana Grande broke a record that was last done by the Beatles and she continues to advocate for mental health for everyone. Twice is one of the kpop groups that brought the genre into the west. While I-DLE is continuing to break boundaries for kpop by speaking about feminism, sexuality, mental health, and empowerment.");
+
+//Question and Answer
+let colorInput = prompt("What is your favorite color?");
+console.log("It is:", colorInput);
